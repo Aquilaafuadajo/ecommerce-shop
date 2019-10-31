7718e83cd80a68801e31d4b2ca8d8cead4003759
Aquila Afuadajo <aquilaafuadajo@gmail.com>
2019-10-31 19:50:21 +0100

    adding header component and links to home, shop page, and contact and also changed the font-family

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import ShopPage from './pages/shop/shop.compnents'
+import ShopPage from './pages/shop/shop.compnents';
+import Header from './components/header/header.component'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
