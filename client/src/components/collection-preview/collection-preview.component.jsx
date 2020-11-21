3589477b70814da998ecd59976c4aecc53640b91
Emmanuel Afuadajo <aquilaafuadajo@gmail.com>
2020-11-21 13:20:56 +0100

    linked each shop preview to their individual category page

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview.styles.scss'
 
-const CollectionPreview = ({title, items}) => {
+const CollectionPreview = ({title, items, routeName, match, history}) => {
+ 
   return ( 
     <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <h1 className='title' onClick={ () => history.push(`${match.url}/${routeName}`)}>{title.toUpperCase()}</h1>
       <div className='preview'>
         {
           items.filter((item, idx) => idx < 4).map(item => (
@@ -17,4 +19,4 @@ const CollectionPreview = ({title, items}) => {
     );
 }
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
